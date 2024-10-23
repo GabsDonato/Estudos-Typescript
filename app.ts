8d2b1c74632ac92c37b44709926c19812afa8517
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-23 13:02:37 -0300

    readonly

--- app.ts
+++ app.ts
@@ -6,8 +6,8 @@ import { CompanyAccount } from './class/CompanyAccount'
 import {PeopleAccount} from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Gabriela', 1234)
-//peopleAccount.deposit()
 console.log(peopleAccount)
 peopleAccount.deposit()
-//const companyAccount : CompanyAccount = new CompanyAccount('DIO', 20)
-//companyAccount.deposit()
+const companyAccount : CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
+
