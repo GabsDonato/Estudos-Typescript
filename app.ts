82042064c3ce38ac7e872cbedc36635850b3a330
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-23 11:38:24 -0300

    Modulos

--- app.ts
+++ app.ts
@@ -2,40 +2,11 @@
 
 // Nome, accountNumber
 //depositar , sacar
-
-abstract class Account {
-    name: string
-    accountNumber: number
-    balance: number = 0
-
-
-    constructor(name: string, accountNumber: number){
-        this.name = name
-        this.accountNumber = accountNumber
-
-    }
-
-    deposit = () => {
-        console.log('Voce depositou')
-    }
-
-    withdraw = () => {
-        console.log('voce sacou')
-    }
-
-    getBalance = () => {
-        console.log(this.balance)
-    }
-}
-
-class PeopleAccount extends Account{
-    doc_id: number
-
-    constructor(doc_id: number, name: string, accountNumber: number){
-        super(name,accountNumber)
-        this.doc_id = doc_id
-    }
-}
+import { CompanyAccount } from './class/CompanyAccount'
+import {PeopleAccount} from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Gabriela', 1234)
-console.log(peopleAccount)
+peopleAccount.deposit()
+
+const companyAccount : CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
