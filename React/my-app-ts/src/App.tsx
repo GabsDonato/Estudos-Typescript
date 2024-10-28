a60729e12877bd4be41b3dfc65b0cac1585062b3
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-28 14:44:19 -0300

    components

---
+++ React/my-app-ts/src/App.tsx
@@ -0,0 +1,16 @@
+import { Card } from './components/Card';
+
+function App() {
+  return (
+    <div>
+     
+     <>
+      <h1>Card</h1>
+      <p>Paragrafo</p>
+     </div>
+     <Card />
+    </>
+  );
+}
+
+export default App;
