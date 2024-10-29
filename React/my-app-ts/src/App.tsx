616cb90c672c456c592cf790ab1c9f92ee813ef1
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-29 20:05:27 -0300

    ajustes aplicação

--- React/my-app-ts/src/App.tsx
+++ React/my-app-ts/src/App.tsx
@@ -1,9 +1,18 @@
 import { Layout } from './components/Layout';
+import styled from 'styled-components';
+
+const Box = styled.div`
+ background-color: orange;
+ border-radius: 25px;
+ padding-left: 15px;
+`
 
 function App() {
   return (
     <Layout>
+      <Box>
       <h1>Faça o login</h1>
+      </Box>
       <label htmlFor='emailInput' >
         Email
       </label>
@@ -14,7 +23,7 @@ function App() {
       </label>
       <input id='passwordInput' type='password'/>
       <button>
-        EntrarS
+        Entrar
       </button>
     </Layout>
   );
