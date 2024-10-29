54a964fb2b4c0a57fb4fed1a15378674ed4132ea
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-10-29 12:37:25 -0300

    Inicializando dio bank

--- React/my-app-ts/src/App.tsx
+++ React/my-app-ts/src/App.tsx
@@ -4,21 +4,7 @@ import { Layout } from './components/Layout';
 function App() {
   return (
     <Layout>
-    <Card 
-    id={1}
-    paragraph='Typescript'
-    details='TS para frontend e backend' 
-    />
-    <Card 
-    id={2} 
-    paragraph='HTML'
-    details='HTML para frontend' 
-    />
-    <Card 
-    id={3} 
-    paragraph='SQL'
-    details='SQL para banco de dados' 
-    />  
+      Dio Bank
     </Layout>
   );
 }
