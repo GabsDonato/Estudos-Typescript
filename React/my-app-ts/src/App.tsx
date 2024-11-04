15a75a5330fdae5554ef220635d3ebc05d87c16a
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-11-04 09:46:13 -0300

    Chara Ui finish

--- React/my-app-ts/src/App.tsx
+++ React/my-app-ts/src/App.tsx
@@ -1,35 +1,8 @@
 import { Layout } from './components/Layout';
 import styled from 'styled-components';
-import { ChakraProvider } from '@chakra-ui/react'
+import { 
+  ChakraProvider,
+   Input
+} from '@chakra-ui/react'
 
-const Box = styled.div`
- background-color: orange;
- border-radius: 25px;
- padding-left: 15px;
-`
 
-function App() {
-  return (
-    <ChakraProvider>
-          <Layout>
-          <Box>
-          <h1>Faça o login</h1>
-          </Box>
-          <label htmlFor='emailInput' >
-            Email
-          </label>
-          <input id='emailInput' type='email' />
-
-          <label htmlFor='passwordInput'>
-            Senha
-          </label>
-          <input id='passwordInput' type='password'/>
-          <button>
-            Entrar
-          </button>
-        </Layout>
-      </ChakraProvider>
-  );
-}
-
-export default App;
